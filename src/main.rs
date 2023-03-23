f1baaf67808cca2e64a16d49a432cc6f96ebea78
Raeki <mkeithley@gmail.com>
2023-03-23 19:39:52 +0900

    test: setup a timer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
-use bevy::app::App;
+use bevy::{
+    app::App,
+    prelude::{Plugin, Res, ResMut, Resource},
+    time::{Time, Timer},
+    DefaultPlugins,
+};
 
-fn main() {
-    App::new().add_system(hello_world).run();
+#[derive(Resource)]
+struct InitTimer(Timer);
+
+fn init(time: Res<Time>, mut timer: ResMut<InitTimer>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        println!("initialized");
+    }
+}
+pub struct InitPlugin;
+
+impl Plugin for InitPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(init);
+    }
 }
 
-fn hello_world() {
-    println!("hello world!");
+fn main() {
+    App::new()
+        .insert_resource(InitTimer(Timer::from_seconds(
+            2.0,
+            bevy::time::TimerMode::Repeating,
+        )))
+        .add_plugins(DefaultPlugins)
+        .add_system(init)
+        .run();
 }
